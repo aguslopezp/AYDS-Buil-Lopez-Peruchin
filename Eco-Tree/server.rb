f1db5bdda6355108638b00f8433dea040598acda
Ivan Peruchin <ivanperuchin12@gmail.com>
2023-05-24 20:32:04 -0300

    agrego vista star

--- Eco-Tree/server.rb
+++ Eco-Tree/server.rb
@@ -85,9 +85,12 @@ class App < Sinatra::Application
   end
   
   get '/' do
-    erb :login
+    erb :start
   end
 
+  get '/login' do
+    erb :login
+  end
 
   post '/login' do
     @user = User.find_by(username: params[:username])
