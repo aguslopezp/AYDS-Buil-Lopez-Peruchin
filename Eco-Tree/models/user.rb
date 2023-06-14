2730dfae51e2a58abe0392dbd3ed84f0432bce8f
aguslopezp <agustinlopezprieto@gmail.com>
2023-06-14 17:57:17 -0300

    Agregamos tests

--- Eco-Tree/models/user.rb
+++ Eco-Tree/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base  
   has_many :questions
   has_many :answers, through: :questions
+  
+  validates :username, presence: true
+  validates :password, presence: true
+  validates :email, presence: true
+  validates :birthdate, presence: true
 end
---
+++ Eco-Tree/spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'sinatra/activerecord'
+require_relative '../../models/init.rb'
+
+describe 'User' do
+  describe 'valid' do
+    describe 'when there is no name' do
+      it 'should be invalid' do
+        #Arrage
+        u = User.new(username: 'usertest',
+                     password: '123',
+                     email:'usertest@email.test',
+                     birthdate: 1999-12-12)
+        #Assert
+        expect(u.valid?).to eq(true)
+      end
+    end
+  end
+end
