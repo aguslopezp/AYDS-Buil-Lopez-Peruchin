eeafe30d20dd27041cd75b7c250d3ed384d24973
Ivan Peruchin <ivanperuchin12@gmail.com>
2023-10-16 11:35:35 -0300

    [#186222302] store funcionando

--- Eco-Tree/server.rb
+++ Eco-Tree/server.rb
@@ -434,6 +434,7 @@ class App < Sinatra::Application
     @user = User.find(user_id)
     @coin = @user.coin
     @item = Item.where(section: 'hoja')
+    @leaf_id = User.find(user_id).leaf_id
    
   
     purchased_item_ids = PurchasedItem.where(user_id: user_id).pluck(:item_id) #busca los items que compro el usuario
@@ -451,7 +452,8 @@ class App < Sinatra::Application
 
   post '/buySkin' do
     request_body = JSON.parse(request.body.read)
-    name = request_body['name']
+    name = request_body['name'] #json
+
     user_id = session[:user_id]
     item_id = Item.find_by(name: name).id
     user = User.find_by(id: user_id)
@@ -460,13 +462,13 @@ class App < Sinatra::Application
     #Si no lo compro lo agrega
     if PurchasedItem.find_by(item_id: item_id, user_id: user_id).nil?
       PurchasedItem.create(user_id: user_id, item_id: item_id)
-
+      
       if (item_price <= user.coin)
         user.discount_coins(item_price)
       end
 
     end
-    #setea la nueva hoja elegida por el usuario
+    #setea la nueva hoja elegida por el usuario, este comprada o no
     user = User.find_by(id: user_id)
     user.update_column(:leaf_id, item_id)
   end
@@ -480,6 +482,8 @@ class App < Sinatra::Application
     @user = User.find(user_id)
     @coin = @user.coin
     @item = Item.where(section: 'fondo')
+    @background_id = User.find(user_id).background_id
+
 
     purchased_item_ids = PurchasedItem.where(user_id: user_id).pluck(:item_id) #busca los items que compro el usuario
     
