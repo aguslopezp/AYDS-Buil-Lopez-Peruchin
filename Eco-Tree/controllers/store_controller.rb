0592b34b166a8c3800fc29e8e04febce34d0bb2c
Ivan Peruchin <ivanperuchin12@gmail.com>
2023-11-02 11:15:37 -0300

    [#186373527] aplico rubocop y modifico cambios necesarios

--- Eco-Tree/controllers/store_controller.rb
+++ Eco-Tree/controllers/store_controller.rb
@@ -1,40 +1,46 @@
+# frozen_string_literal: true
+
+# Esta es la clase del controlador de la tienda.
+# Maneja las rutas de la tienda y la compra de hojas/fondos denominados Items.
 class StoreController < Sinatra::Application
-  before ['/store', '/buyItem'] do 
-    if session[:user_id].nil?
-      redirect '/' 
-    end
+  before ['/store', '/buyItem'] do
+    # Verifica si el usuario ha iniciado sesion antes de acceder a ciertas rutas.
+    redirect '/' if session[:user_id].nil?
   end
 
+  # Ruta para mostrar la tienda.
   get '/store' do
     @user = current_user
     @coin = @user.coin
     erb :store
   end
 
-  get '/buyItem' do  
+  # Ruta para comprar un item de la tienda.
+  get '/buyItem' do
     @user = current_user
     @coin = @user.coin
     @item_selected = params[:item]
     @item = Item.where(section: @item_selected)
 
-    if (@item_selected == 'hoja')
+    if @item_selected == 'hoja'
       @item_selected_id = @user.leaf_id
-    elsif (@item_selected == 'fondo')
+    elsif @item_selected == 'fondo'
       @item_selected_id = @user.background_id
     end
- 
+
     purchased_item_ids = PurchasedItem.where(user_id: @user.id).pluck(:item_id)
-    
-    @item_comprados = {} 
+
+    @item_comprados = {}
     @item_price = {}
     @item.each do |item|
-      @item_comprados[item.id] = purchased_item_ids.include?(item.id) 
-      @item_price[item.id] = item.price 
+      @item_comprados[item.id] = purchased_item_ids.include?(item.id)
+      @item_price[item.id] = item.price
     end
-    
+
     erb :buyItem
   end
 
+  # Metodo POST para comprar un elemento de la tienda.
   post '/buyItem' do
     user = current_user
 
@@ -44,11 +50,10 @@ class StoreController < Sinatra::Application
 
     user.buy_item(item.id, item.price)
     user.set_item_in_user(params[:item], item.id)
-    
-  end
-  
-  def current_user 
-    return User.find(session[:user_id]) if session[:user_id]
   end
 
+  # Obtiene el usuario actual en la sesion.
+  def current_user
+    return User.find(session[:user_id]) if session[:user_id]
+  end
 end
